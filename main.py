aeca41b7d8faf1499ee4488fabefc75f8466883b
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2023-12-25 16:02:20 +0300

    Optimized & fixed Code

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 import random, string
 
-def get_characters():
-    characters = string.ascii_letters + string.digits + string.punctuation
-    character_set = []
-    for i in characters:
-         if len(character_set) != 8:
-             character_set.append(random.choice(characters))
-    return character_set
+def generatePassword():
+    upp_1 = chr(random.randint(ord('A'), ord('Z')))
+    upp_2 = chr(random.randint(ord('A'), ord('Z')))
+    
+    low_1 = chr(random.randint(ord('a'), ord('z')))
+    low_2 = chr(random.randint(ord('a'), ord('z')))
+
+    dig_1 = random.choice(string.digits)
+    dig_2 = random.choice(string.digits)
+    
+    p_1 = random.choice(string.punctuation)
+    p_2 = random.choice(string.punctuation)
+    
+    characters = [upp_1,upp_2,dig_1,dig_2,p_1,p_2,low_1,low_2]
+    
+    random.shuffle(characters)
+    return characters
 
 def duplicates(numOfDup):
     i = 0
     while i < numOfDup:
-        print("You Password is ", ''.join(password_restriction()))
-        print('\n')
+        result = ''.join(generatePassword()) 
+        print("Password: ",''.join(result))
         i += 1
-def password_restriction():
-    text = get_characters()
-    result = ''
-    countUpper = countLower = countDigit = countPunc = 0
-    for i in range(len(text)):
-        if text[i].isupper() and countUpper < 2:
-            countUpper += 1
-            result += text[i]
-        elif text[i].islower() and countLower < 2:
-            countLower += 1
-            result += text[i]
-        elif text[i].isdigit() and countDigit < 2:
-            countDigit += 1
-            result += text[i]
-        elif text[i] in string.punctuation and countPunc < 2:
-            countPunc += 1
-            result += text[i]
-    return result
-
 if __name__ == '__main__':
     duplicates(3)
